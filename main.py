9b1aec87612e7ff4483ff4b927e19342096bf013
Oyekanmi lekan <oyekanmiakande@gmail.com>
2023-12-22 01:00:31 +0100

    Implement models and main app

--- main.py
+++ main.py
@@ -1,3 +1,47 @@
 """
 main.py
 """
+from models import Expense, ExpenseDB
+# import pretty_table_printer
+
+
+
+
+if __name__ == "__main__":
+    print("Welcome to my Expense Tracker APP.")
+    print("#"*20)
+    
+    # create an object of expense
+    expense1 = Expense("Enrolled into AltSchool", "250000")
+    expense2 = Expense("Get a Pen", "200")
+    expense3 = Expense("Bought a House", "5500000")
+    expense4 = Expense("Apply for Visa", "35000")
+    expense5 = Expense("Data Subscription", "10000")
+    
+    all_expenses = [expense1, expense2, expense3, expense4, expense5]
+    
+    # add an expense to the database
+    edb = ExpenseDB()
+    print("Initializing Database")
+    print(edb.expense_db)
+    
+    print("Add New Expense")
+    edb.add_expense(expense1)
+    print(edb.expense_db)
+    
+    print("Add another Expense")
+    edb.add_expense(expense2)
+    print(edb.expense_db)
+    
+    # print("Remove an Expense")
+    # edb.remove_expense(expense2.id)
+    # print(edb.expense_db)
+    
+    # print("Get an Expense")
+    # print(edb.get_expense_by_id(expense2.id))
+    
+    print("Get an Expense")
+    print(edb.get_expense_by_title(expense1.title))
+    
+    
+  
--- models.py
+++ models.py
@@ -1,3 +1,107 @@
 """
 models.py
 """
+from uuid import uuid4
+from datetime import datetime
+
+
+
+class Expense():
+    
+    # constructor
+    def __init__(self, title, amount):
+        # instance variables
+        self.id = repr(uuid4())
+        self.title = title
+        self.amount = amount
+        self.created_at = repr(datetime.now())
+        self.updated_at = repr(datetime.now())
+        
+    
+    # instance methods    
+    def update(self,new_title=None, new_amount=None):
+        """
+        Allows updating the title and/or amount, updating the updated_at timestamp.
+        """
+        self.title = new_title if new_title != None else self.title 
+        self.amount = new_amount if new_amount != None else self.amount
+        self.updated_at = repr(datetime.utcnow())
+        
+        return f"Expense with ID:{self.id} successfully updated."
+            
+
+    
+    def to_dict(self,):
+        """
+        Returns a dictionary representation of the expense.
+        """
+        expense_dict = {'id':self.id, 
+                        'title':self.title,
+                        'amount':self.amount,
+                        'created_at':self.created_at,
+                        'updated_at':self.updated_at}
+        return expense_dict
+    
+    
+    def __repr__(self):
+        """
+        Return a string represntation of the object
+        """
+        class_name = type(self).__name__
+        return f"{class_name}(title={self.title!r}, amount={self.amount!r})"
+
+
+class ExpenseDB():
+    # constructor
+    def __init__(self):
+        self.expense_db = list()
+        
+    # instance methods
+    def add_expense(self, expense):
+        """
+        Allows to add an expense.
+        """
+        self.expense_db.append(expense)
+        return f"An Expense with {expense.id} added successfully"
+    
+    def remove_expense(self, id):
+        """
+        Allows to remove an expense.
+        """
+        self.expense_db = [expense for expense in self.expense_db if expense.id != id]
+        return f"An Expense with {id} removed successfully"
+    
+    def get_expense_by_id(self, id):
+        """
+        Retrieves an expense by ID.
+        """
+        expense = [expense for expense in self.expense_db if expense.id == id]
+        if len(expense) == 0:
+            return None
+        
+        return expense[0]
+    
+    def get_expense_by_title(self, title):
+        """
+        Retrieves expenses by title.
+        """
+        expense = [expense for expense in self.expense_db if expense.title == title]
+        if len(expense) == 0:
+            return None
+        
+        return expense[0]
+        
+    def to_dict(self):
+        """
+        Returns a list of dictionaries representing expenses.
+        """
+        
+        pass
+    
+    def __repr__(self):
+        """
+        Return a string represntation of the object
+        """
+        class_name = type(self).__name__
+        return f"{class_name}"
+    
